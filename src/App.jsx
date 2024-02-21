f1e28c08e754aac6d2fe10e6bc5e50006b1e67be
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-02-22 01:16:51 +0530

    implementing the hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LayOut from './LayOut.jsx';
+import Home from './Components/Home/Home.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+import Job from './Components/Job/Job.jsx';
+import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      hello
+     <Routes>
+        <Route path="/" element={<LayOut />}>
+          <Route index element={<Home />} />
+          <Route path="job" element={<Job />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
-  )
+     
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
 
-import{
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import LayOut from './LayOut.jsx'
-import Home from './Components/Home/Home.jsx'
-import Contact from './Components/Contact/Contact.jsx'
-import Job from './Components/Job/Job.jsx'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={ <LayOut/> }>
-      <Route path="" element={ <Home/> }/>
-      <Route path="job" element={ <Job/> }/>
-      <Route path="contact" element={ <Contact/> }/>
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
